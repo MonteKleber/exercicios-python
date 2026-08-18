22e2052bdbf226c81bd0e1fb746bb26e9e3741fd
Kleber Montenegro <montenegrokleber7@gmail.com>
2026-08-18 13:27:51 -0400

    Finalizado o exercicio de número primo

--- nivel-2/numeroPrimo.py
+++ nivel-2/numeroPrimo.py
@@ -1,14 +1,21 @@
 numero = int(input ("Informe o número:\n")) #SOLICITA AO USUÁRIO QUE INFORME O NÚMERO
-raiz = pow(numero, 0.5) #DETERMINA A RAIZ QUADRADA
 cont = 0 #DEFINE UM CONTADOR
 acum = 0 #DEFINE UM ACUMULADOR
 
-while cont <= raiz: #ESTRUTURA CONDICIONAL PARA TESTAR OS VALORES
-    cont = cont+ 1
-    resultado = numero % cont #DIVIDE O NÚMERO QUE FOI DEFINIDO PELO USUÁRIO PELO CONTADOR 
-    if resultado == 0: #SE O RESTO DA DIVISÃO FOR DIFERENTE DE 0 ENTÃO...
-        acum += 1
-if acum == 1: #CASO O ACUMULADOR SEJA IGUAL A 1 ENTÃO ELE EXIBE É QUE É UM NÚMERO PRIMO.
-    print("O número é primo!")
-else:
+if (numero == 0 or numero == 1):
     print ("O número não é primo!")
+elif (numero == 2):
+    print ("O número é primo")
+elif (numero < 0):
+    print ("Erro: Não existem números primos negativos!")
+else:
+    raiz = pow(numero, 0.5) #DETERMINA A RAIZ QUADRADA
+    while cont <= raiz: #ESTRUTURA CONDICIONAL PARA TESTAR OS VALORES
+        cont = cont+ 1
+        resultado = numero % cont #DIVIDE O NÚMERO QUE FOI DEFINIDO PELO USUÁRIO PELO CONTADOR 
+        if resultado == 0: #SE O RESTO DA DIVISÃO FOR IGUAL A 0...
+            acum += 1
+    if acum == 1: #CASO O ACUMULADOR SEJA IGUAL A 1 ENTÃO ELE EXIBE É QUE É UM NÚMERO PRIMO.
+        print("O número é primo!")
+    elif acum > 1:
+        print ("O número não é primo!") 
