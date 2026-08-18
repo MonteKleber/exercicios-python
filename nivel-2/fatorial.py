0f9a59fc754c33efcbd346530e45516dcf072106
Kleber Montenegro <montenegrokleber7@gmail.com>
2026-08-18 08:52:16 -0400

    Corrige o tratamento de números negativos no fatorial

--- nivel-2/fatorial.py
+++ nivel-2/fatorial.py
@@ -2,8 +2,10 @@ numero = int(input("Digite um número: "))
 fatorial = 1
 contador = numero
 
-while contador > 0:
-    fatorial *= contador # *= é equivalente a: fatorial = fatorial * contador
-    contador -= 1
-
-print(f"O fatorial de {numero} é {fatorial}")
+if numero >= 0:
+    while contador > 0:
+            fatorial *= contador # *= é equivalente a: fatorial = fatorial * contador
+            contador -= 1
+    print(f"O fatorial de {numero} é {fatorial}")
+else:
+      print("Não existe fatorial para números negativos")
