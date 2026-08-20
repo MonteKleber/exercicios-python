8b0ebac8348134808037186775a2d5b07d57f5eb
Kleber Montenegro <montenegrokleber7@gmail.com>
2026-08-20 19:44:48 -0400

    exercicio de autenticação de senha

---
+++ nivel-3/verificarSenha.py
@@ -0,0 +1,13 @@
+senha = 'euamoasofia' #define uma senha
+contador = 0
+
+while contador < 3:
+    tentativa = input("Digite sua senha: \n") #solicita que o usuário faça uma tentativa
+    if tentativa == senha: #caso a senha seja a certa, então retorna a mensagem e sai do while
+        print ("Senha correta!") 
+        break
+    else:
+        print ("Senha incorreta!") #caso a senha seja errada adiciona mais um no contador
+        contador += 1
+        if contador == 3:
+            print("Número de tentativas chegou ao limite!")
